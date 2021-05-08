5e0e2bd66f1f8cdb438f5aa2e4b08737f9b5e21e
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2021-05-08 17:56:17 +0900

    [wip] revised

--- components/InputProvider.tsx
+++ components/InputProvider.tsx
@@ -1,19 +1,27 @@
 import React, { useReducer, useContext, createContext } from 'react'
 //import { theme } from '../../styles/theme'
 import { useForm } from '../hooks/useForm'
-
+const INPUT = 'input'
 type InputProviderProps = {
   //   element: type
   children: any
 }
 const InputStateContext = createContext({ text: "" })
-const InputDispatchContext = createContext(null)
-
+const InputDispatchContext = createContext()
+const textReducer = (state, action) => {
+  switch (action.type) {
+    case INPUT:
+      return { ...state, text: action.value }
+    default:
+      return state
+  }
+}
 export const InputProvider: React.FC<InputProviderProps> = ({
   children,
 }: InputProviderProps): JSX.Element => {
   console.log("きてる")
-  const [state, dispatch] = useForm('')
+  //const [state, dispatch] = useForm('')
+  const [state, dispatch] = useReducer(textReducer, {text: ''})
   return (
     <InputDispatchContext.Provider value={dispatch}>
       <InputStateContext.Provider value={state}>
