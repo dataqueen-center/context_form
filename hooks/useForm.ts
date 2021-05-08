283470e46a1b947b4fa1e1a60dbfbe3dfc872999
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2021-05-08 23:46:30 +0900

    remove unused

--- hooks/useForm.ts
+++ hooks/useForm.ts
@@ -6,7 +6,6 @@ export const useForm = (initialState: string) => {
   const textReducer = (state, action) => {
     switch (action.type) {
       case INPUT:
-        //return { ...state, text: action.value }
         return action.value
       default:
         return state
@@ -14,10 +13,8 @@ export const useForm = (initialState: string) => {
   }
   const initTextState = (initialText: string) => {
     console.log("きてる")
-    //return { text: initialText }
     return initialText
   }
   const [state, dispatch] = useReducer(textReducer, initialState, initTextState)
-  //const [state, dispatch] = useReducer(textReducer, {text: ""})
   return [state, dispatch]
 }
